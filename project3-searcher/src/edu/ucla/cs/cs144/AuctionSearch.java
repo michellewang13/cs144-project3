71da0524be44aa48cc57b1fa0632b8879c3d9f2e
Michelle Wang <michellewang13@ucla.edu>
2017-02-16 17:14:52 -0800

    finished part B4

--- project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
+++ project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -1,5 +1,9 @@
 package edu.ucla.cs.cs144;
 
+import java.util.*;
+import java.sql.*;
+import java.util.*;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
@@ -61,7 +65,6 @@ public class AuctionSearch implements IAuctionSearch {
 			Query queryObj = parser.parse(query);
 
 			TopDocs results = searcher.search(queryObj, numResultsToSkip + numResultsToReturn);
-
 			ScoreDoc[] hits = results.scoreDocs;
 			int numHits = hits.length;
 
@@ -69,13 +72,13 @@ public class AuctionSearch implements IAuctionSearch {
 			if (numHits < numResultsToSkip)
 				numResultsToReturn = 0;
 			else
-				numResultsToReturn = Math.min(numHits, numResultsToReturn);
+				numResultsToReturn = Math.min(numHits - numResultsToSkip, numResultsToReturn);
 
 			SearchResult[] searchResults = new SearchResult[numResultsToReturn];
 
 			for (int i = numResultsToSkip, j = 0; i < numResultsToSkip + numResultsToReturn; i++, j++) {
 				Document doc = this.getDocument(hits[i].doc);
-				searchResults[j] = new SearchResult(doc.get("ItemID"), doc.get("name"));
+				searchResults[j] = new SearchResult(doc.get("ItemID"), doc.get("Name"));
 			}
 		
 		return searchResults;
@@ -100,8 +103,11 @@ public class AuctionSearch implements IAuctionSearch {
 			Connection con = DbManager.getConnection(true);
 
 			// Do basic search
+			int index = 0;
+
 			int originalNumResultsToReturn = numResultsToReturn;
-			SearchResult[] results = basicSearch(query, 0, numResultsToReturn);
+			SearchResult[] results = basicSearch(query, index, numResultsToReturn);
+			index += results.length;
 
 			// Get region bounds as a MySQL geometry
 			String regionBounds = this.getRegionBounds(region.getLx(), region.getLy(), region.getRx(), region.getRy());
@@ -113,6 +119,7 @@ public class AuctionSearch implements IAuctionSearch {
 
 			// Keep basic searching and spatial searching until numResultsToReturn results have been added
 			while (numResultsToReturn > 0) {
+				System.out.println(String.format("numResultsToReturn: %d", numResultsToReturn));
 				// Check each item found by basic search if in region
 				for (int i = 0; i < results.length; i++) {
 					SearchResult item = results[i];
@@ -127,7 +134,7 @@ public class AuctionSearch implements IAuctionSearch {
 							numResultsToSkip--;
 						
 						} else if (numResultsToReturn > 0) {
-							System.out.println(numResultsToReturn);
+							//System.out.println(numResultsToReturn);
 							searchResults.add(item);
 							numResultsToReturn--;
 						} else {
@@ -139,7 +146,11 @@ public class AuctionSearch implements IAuctionSearch {
 					checkPositionRS.close();
 				}
 
-				results = basicSearch(query, results.length, originalNumResultsToReturn);
+				results = basicSearch(query, index, originalNumResultsToReturn);
+				index += results.length;
+
+				if (results.length <= 0)
+					break;
 			}
 
 			checkPosition.close();
@@ -162,10 +173,247 @@ public class AuctionSearch implements IAuctionSearch {
 	}
 
 	public String getXMLDataForItemId(String itemId) {
-		// TODO: Your code here!
-		return "";
+        Connection conn = null;
+        String xmlData = "";
+
+        try {
+            conn = DbManager.getConnection(true);
+            Statement stmt = conn.createStatement();
+
+            Map<String, String> itemsMap = getItemData(itemId, stmt);
+            if (itemsMap == null) {
+                return "";
+            }
+
+            String name = itemsMap.get("Name");
+            String location = itemsMap.get("Location");
+            String country = itemsMap.get("Country");
+            String latitude = itemsMap.get("Latitude");
+            String longitude = itemsMap.get("Longitude");
+            String userID = itemsMap.get("UserID");
+            String description = itemsMap.get("Description");
+            String started = itemsMap.get("Started");
+            String ends = itemsMap.get("Ends");
+            String currently = itemsMap.get("Currently");
+            String buyPrice = itemsMap.get("Buy_Price");
+            String firstBid = itemsMap.get("First_Bid");
+            String numBids = itemsMap.get("NumBids");
+
+            ArrayList<String> categoryList = getCategoryData(itemId, stmt);
+            String sellerRating = getSellerData(userID, stmt);
+            ArrayList<Map<String, String>> bidsList = getBidData(itemId, stmt);
+
+            // format into XML
+            String categories, bids, locationAttributes;
+            categories = formatCategories(categoryList);
+            bids = formatBids(bidsList);
+            locationAttributes = (latitude.equals("") && longitude.equals(""))? "" : formatLatitudeLongitude(latitude, longitude);
+            buyPrice = buyPrice.equals("0.00")? "" : "  <Buy_Price>$" + buyPrice + "</Buy_Price>\n";
+
+            if (bids.isEmpty()) {
+            	xmlData = String.format(
+	                            "<Item ItemID=\"%s\">\n" +
+	                            "  <Name>%s</Name>\n" +
+	                            "%s" +
+	                            "  <Currently>$%s</Currently>\n" +
+	                            "%s" +
+	                            "  <First_Bid>$%s</First_Bid>\n" +
+	                            "  <Number_of_Bids>%s</Number_of_Bids>\n" +
+	                            "  <Bids />\n" +
+	                            "  <Location%s>%s</Location>\n" +
+	                            "  <Country>%s</Country>\n" +
+	                            "  <Started>%s</Started>\n" +
+	                            "  <Ends>%s</Ends>\n" +
+	                            "  <Seller Rating=\"%s\" UserID=\"%s\" />\n" +
+	                            "  <Description>%s</Description>\n" +
+	                            "</Item>",
+	                    itemId, name, categories, currently, buyPrice, firstBid,
+	                    numBids, locationAttributes, location, country,
+	                    started, ends, sellerRating, userID, description
+	            );
+            }
+            else {
+	            xmlData = String.format(
+	                            "<Item ItemID=\"%s\">\n" +
+	                            "  <Name>%s</Name>\n" +
+	                            "%s" +
+	                            "  <Currently>$%s</Currently>\n" +
+	                            "%s" +
+	                            "  <First_Bid>$%s</First_Bid>\n" +
+	                            "  <Number_of_Bids>%s</Number_of_Bids>\n" +
+	                            "  <Bids>\n%s  </Bids>\n" +
+	                            "  <Location%s>%s</Location>\n" +
+	                            "  <Country>%s</Country>\n" +
+	                            "  <Started>%s</Started>\n" +
+	                            "  <Ends>%s</Ends>\n" +
+	                            "  <Seller Rating=\"%s\" UserID=\"%s\" />\n" +
+	                            "  <Description>%s</Description>\n" +
+	                            "</Item>",
+	                    itemId, name, categories, currently, buyPrice, firstBid,
+	                    numBids, bids, locationAttributes, location, country,
+	                    started, ends, sellerRating, userID, description
+	            );
+			}
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return xmlData;
+	}
+
+	public String formatCategories(ArrayList<String> categoryList) {
+		String categories = "";
+		for (String category : categoryList) {
+			categories += String.format("  <Category>%s</Category>\n", category);
+		}
+		return categories;
+	}
+
+	public String formatBids(ArrayList<Map<String, String>> bidList) {
+		String bids = "";
+		for (Map<String, String> bidMap : bidList) {
+			String detailedLocation = "";
+			String location = bidMap.get("Location");
+			String country = bidMap.get("Country");
+
+			if (location != null) {
+				detailedLocation += String.format("        <Location>%s</Location>\n", location);
+			}
+			if (country != null) {
+				detailedLocation += String.format("        <Country>%s</Country>\n", country);
+			}
+
+			bids += String.format("    <Bid>\n      <Bidder Rating=\"%s\" UserID=\"%s\">\n" + 
+									"%s" + 
+									"      </Bidder>\n" + 
+									"      <Time>%s</Time>\n" + 
+									"      <Amount>$%s</Amount>\n" + 
+									"  </Bid>\n", bidMap.get("Bidder_Rating"), 
+									bidMap.get("UserID"), detailedLocation, bidMap.get("Time"), bidMap.get("Amount"));
+		}
+		return bids;
+	}
+
+	public String formatLatitudeLongitude(String latitude, String longitude) {
+		String s = "";
+		if (latitude != null && longitude != null) {
+			s = String.format(" Latitude=\"%s\" Longitude=\"%s\"", latitude, longitude);
+		}
+		return s;
+	}
+
+	public Map<String, String> getItemData(String itemId, Statement stmt) throws SQLException {
+		String name, currently, buyPrice, firstBid, numBids, location, country, latitude, longitude, started, ends, userId, description;
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Item WHERE ItemID = " + itemId);
+
+		if (!rs.next()) {
+			return null;
+		}
+
+		name = escapeChar(rs.getString("Name"));
+		currently = String.format("%.2f", rs.getDouble("Currently"));
+		buyPrice = String.format("%.2f", rs.getDouble("Buy_Price"));
+		firstBid = String.format("%.2f", rs.getDouble("First_Bid"));
+		numBids = Integer.toString(rs.getInt("Number_of_Bids"));
+		
+		location = escapeChar(rs.getString("Location"));
+		country = escapeChar(rs.getString("Country"));
+		latitude = rs.getString("Latitude");
+		longitude = rs.getString("Longitude");
+		userId = escapeChar(rs.getString("UserID"));
+		description = escapeChar(rs.getString("Description"));
+
+		SimpleDateFormat outputFormat = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
+		started = outputFormat.format(rs.getTimestamp("Started"));
+		ends = outputFormat.format(rs.getTimestamp("Ends"));
+
+		Map<String, String> itemsMap = new HashMap<String, String>();
+		itemsMap.put("Name", name);
+		itemsMap.put("Currently", currently);
+		itemsMap.put("Buy_Price", buyPrice);
+		itemsMap.put("First_Bid", firstBid);
+		itemsMap.put("Location", location);
+		itemsMap.put("Country", country);
+		itemsMap.put("Latitude", latitude);
+		itemsMap.put("Longitude", longitude);
+		itemsMap.put("Started", started);
+		itemsMap.put("Ends", ends);
+		itemsMap.put("UserID", userId);
+		itemsMap.put("Description", description);
+		itemsMap.put("NumBids", numBids);
+
+		return itemsMap;
+	}
+
+	public ArrayList<String> getCategoryData(String itemId, Statement stmt) throws SQLException {
+		ResultSet rs = stmt.executeQuery("SELECT Category FROM Category WHERE ItemID = " + itemId);
+
+		ArrayList<String> categoryList = new ArrayList<String>();
+		while(rs.next()) {
+			categoryList.add(escapeChar(rs.getString("Category")));
+		}
+
+		return categoryList;	
+	} 
+
+	public ArrayList<Map<String, String>> getBidData(String itemId, Statement stmt) throws SQLException {
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Bid WHERE ItemID = " + itemId);
+
+		ArrayList<Map<String, String>> bidList = new ArrayList<Map<String, String>>();
+
+		while (rs.next()) {
+			Map<String, String> bidMap = new HashMap<String, String>();
+			String userId = escapeChar(rs.getString("UserID"));
+			bidMap.put("UserID", userId);
+
+			SimpleDateFormat outputFormat = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
+			String time = outputFormat.format(rs.getTimestamp("Time")); 
+			bidMap.put("Time", time);
+			bidMap.put("Amount", String.format("%.2f", rs.getFloat("Amount")));
+			bidList.add(bidMap);
+		}
+
+		for (Map<String, String> bid : bidList) {
+			String userId = bid.get("UserID");
+			bid.putAll(getBidderData(userId, stmt));
+		}
+
+		return bidList;
 	}
 	
+	public String getSellerData(String userId, Statement stmt) throws SQLException {
+		int rating = 0;
+		ResultSet rs = stmt.executeQuery("SELECT Seller_Rating FROM User WHERE UserID = '" + userId + "'");
+
+		while (rs.next()) {
+			rating = rs.getInt("Seller_Rating");
+		}
+
+		return Integer.toString(rating);
+	}
+
+	public Map<String, String> getBidderData(String userId, Statement stmt) throws SQLException {
+		ResultSet rs = stmt.executeQuery("SELECT * FROM User WHERE UserID = '" + userId + "'");
+		Map<String, String> bidderMap = new HashMap<String, String>();
+
+		while (rs.next()) {
+			bidderMap.put("Bidder_Rating", Integer.toString(rs.getInt("Bidder_Rating")));
+			bidderMap.put("Location", escapeChar(rs.getString("Location")));
+			bidderMap.put("Country", escapeChar(rs.getString("Country")));
+		}
+
+		return bidderMap;
+	}
+
+	private String escapeChar(String s) {
+        s = s.replace("&", "&amp;");
+        s = s.replace("\"", "&quot;");
+        s = s.replace("'", "&apos;");
+        s = s.replace("<", "&lt;");
+        s= s.replace(">", "&gt;");
+        return s;
+    }
+
 	public String echo(String message) {
 		return message;
 	}
--- project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -16,27 +16,28 @@ public class AuctionSearchTest {
 		String reply = as.echo(message);
 		System.out.println("Reply: " + reply);
 		
-		String query = "superman";
-		SearchResult[] basicResults = as.basicSearch(query, 0, 2000);
-		System.out.println("Basic Seacrh Query: " + query);
-		System.out.println("Received " + basicResults.length + " results");
-		// for(SearchResult result : basicResults) {
+		// String query = "kitchenware";
+		// SearchResult[] basicResults = as.basicSearch(query, 0, 2000);
+		// System.out.println("Basic Seacrh Query: " + query);
+		// System.out.println("Received " + basicResults.length + " results");
+		
+		//  for(SearchResult result : basicResults) {
 		// 	System.out.println(result.getItemId() + ": " + result.getName());
 		// }
 		
-		// SearchRegion region =
-		//     new SearchRegion(33.774, -118.63, 34.201, -117.38); 
-		// SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
-		// System.out.println("Spatial Seacrh");
-		// System.out.println("Received " + spatialResults.length + " results");
-		// for(SearchResult result : spatialResults) {
-		// 	System.out.println(result.getItemId() + ": " + result.getName());
-		// }
+		/*SearchRegion region =
+		    new SearchRegion(33.774, -118.63, 34.201, -117.38); 
+		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 300);
+		System.out.println("Spatial Seacrh");
+		System.out.println("Received " + spatialResults.length + " results");
+		for(SearchResult result : spatialResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
+		}*/
 		
-		// String itemId = "1497595357";
-		// String item = as.getXMLDataForItemId(itemId);
-		// System.out.println("XML data for ItemId: " + itemId);
-		// System.out.println(item);
+		String itemId = "1044078197";
+		String item = as.getXMLDataForItemId(itemId);
+		System.out.println("XML data for ItemId: " + itemId);
+		System.out.println(item);
 
 		// Add your own test here
 	}
